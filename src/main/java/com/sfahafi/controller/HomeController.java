b5f13be3f528659e918e5d11694ea1339cd8164d
sfahafi <sfahafi18@gmail.com>
2021-11-19 23:16:00 -0300

    Aplicando atributos al model y listas (List) en el controller

--- src/main/java/com/sfahafi/controller/HomeController.java
+++ src/main/java/com/sfahafi/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.sfahafi.controller;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,10 +11,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
+	@GetMapping("/listado")
+	public String mostrarListado(Model model) {
+		List<String> lista = new LinkedList<String>();
+		lista.add("Ingeniero de Sistemas");
+		lista.add("Auxiliar de contabilidad");
+		lista.add("vendedor");
+		lista.add("Arquitecto");
+		
+		model.addAttribute("empleos", lista);
+		
+		return "listado";
+		
+	}
+	
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
+		/*
 		model.addAttribute("mensaje", "Bienvenidos a empleos app");
 		model.addAttribute("fecha", new Date());
+		*/
+		
+		String nombre = "Auxiliar de contabilidad";
+		Date fechaPub = new Date();
+		double salario = 9000.0;
+		boolean vigente = true;
+		
+		model.addAttribute("nombre", nombre);
+		model.addAttribute("fecha", fechaPub);
+		model.addAttribute("salario", salario);
+		model.addAttribute("vigente", vigente);
+		
 		return "home";
 	}
 	
