c14ab7cf3ccdf8e1acee20c3964d44cce3c3280b
sfahafi <sfahafi18@gmail.com>
2021-11-30 21:59:23 -0300

    Usando anotacion RequestMapping a nivel de clase y metodo

---
+++ src/main/java/com/sfahafi/controller/CategoriasController.java
@@ -0,0 +1,30 @@
+package com.sfahafi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="categorias")
+public class CategoriasController {
+
+	// @GetMapping("/index")
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String mostrarIndex(Model model) {
+		return "categorias/listCategorias";
+	}
+	
+	// @GetMapping("/create")
+	@RequestMapping(value="/create", method=RequestMethod.GET)
+	public String crear() {
+		return "categorias/formCategorias";
+	}
+	
+	// @PostMapping("/save")
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String guardar() {
+		return "categorias/listCategorias";
+	}
+	
+}
