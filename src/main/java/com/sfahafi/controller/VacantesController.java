c2204f7f882c7926faf677385e1ffb24a85a4a90
sfahafi <sfahafi18@gmail.com>
2021-12-01 21:46:31 -0300

    Usando la anotacion RequestParam en peticion HTTP GET y POST

--- src/main/java/com/sfahafi/controller/VacantesController.java
+++ src/main/java/com/sfahafi/controller/VacantesController.java
@@ -5,11 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/vacantes")
 public class VacantesController {
 
+	@GetMapping("/delete")
+	public String eliminar(@RequestParam("id") int idVacante, Model model) {
+		System.out.println("Borrando vacante con id: " + idVacante);
+		model.addAttribute("id", idVacante);
+		return "mensaje";
+	}
+	
+	
 	@GetMapping("/view/{id}")
 	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
 		System.out.println("IdVacante" + idVacante);
