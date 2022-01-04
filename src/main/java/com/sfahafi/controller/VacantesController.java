958917f039293dc2370fb5b5eece1931e5b1892b
Sharbel <sfahafi18@gmail.com>
2022-01-04 12:34:16 -0300

    Agregando Formulario para crear Vacantes

--- src/main/java/com/sfahafi/controller/VacantesController.java
+++ src/main/java/com/sfahafi/controller/VacantesController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,6 +18,28 @@ public class VacantesController {
 
 	@Autowired
 	private I_VacantesService serviceVacantes;
+	
+	@GetMapping("/create")
+	public String crear() {
+		return "vacantes/formVacante";
+	}
+	
+	@PostMapping("/save")
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion,
+			@RequestParam("estatus") String estatus, @RequestParam("fecha") String fecha,
+			@RequestParam("destacado") int destacado, @RequestParam("salario") Double salario,
+			@RequestParam("detalles") String detalles){
+		
+		System.out.println("Nombre de Vacante: " + nombre);
+		System.out.println("Descripcion: " + descripcion);
+		System.out.println("Estatus: " + estatus);
+		System.out.println("Fecha de Publicacion: " + fecha);
+		System.out.println("Destacado: " + destacado);
+		System.out.println("Salario: " + salario);
+		System.out.println("Detalles: " + detalles);
+		
+		return "vacantes/listVacantes";
+	}
 	
 	@GetMapping("/delete")
 	public String eliminar(@RequestParam("id") int idVacante, Model model) {
